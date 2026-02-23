7f54707a68fc5ac0b2be96dbefd41eef8599ecf3
João Fernandes <jotapecosta07@gmail.com>
2026-02-23 02:46:09 -0300

    2 basic projects that I created in Java
    2 basics projetcs from Guanabara's course of Basic Java.
    With comments about me logic

---
+++ ProjetosEstudos-Java-Basico/src/estudos/horadosistema/HoraDoSistema.java
@@ -0,0 +1,21 @@
+/* Atividade Prática do curso básico de Java do Guanabara */
+
+package estudos.horadosistema;
+
+// importei date da biblioteca.
+import java.util.Date;
+
+public class HoraDoSistema {
+    static void main() {
+
+        // relogio é um objeto criado, o =new indica isso
+        Date relogio = new Date();
+
+        // apenas prints normais.
+        System.out.print("A hora do sistema é: ");
+        System.out.println(relogio);
+    }
+}
+/* No curso é ensinado a usar a função .toString, porém identifiquei que
+não é obrigatório.
+ */
---
+++ ProjetosEstudos-Java-Basico/src/estudos/idiomas/sistema/IdiomasDoSistema.java
@@ -0,0 +1,26 @@
+/*
+Desafio Básico do curso
+ */
+
+package estudos.idiomas.sistema;
+
+// Importei Locale da biblioteca
+import java.util.Locale;
+
+public class IdiomasDoSistema {
+    static void main() {
+
+        // Aqui puxei direto o method .getDefault() da class Locale
+        Locale idioma = Locale.getDefault();
+
+        // prints normais
+        System.out.print("O Idioma utilizado no sistema é: ");
+        System.out.print(idioma.getDisplayLanguage());
+    }
+}
+
+/*
+Neste desafio utilizei ajuda da IA para saber qual biblioteca importar
+eu usei a lógica da atividade prática, apenas perguntei para IA, o que eu deveria importar.
+Aprendi que cada class importada da biblioteca tem seus methods prontos
+ */
