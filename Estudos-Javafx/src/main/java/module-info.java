3658444d5734dcb0574fd536b27498fac7063ba6
João Fernandes <jotapecosta07@gmail.com>
2026-02-24 23:19:00 -0300

    upd

--- Estudos-Javafx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.testsjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.testsjavafx to javafx.fxml;
-    exports com.example.testsjavafx;
-}
