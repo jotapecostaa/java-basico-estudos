41a77d1920504d853fd17407a539c6891f6131a6
João Costa <jotapecosta07@gmail.com>
2026-02-27 11:34:47 -0300

    add a gui swing with calc

--- ProjetosEstudos-Java-Basico/src/estudos/testesswing/Calc.java
+++ ProjetosEstudos-Java-Basico/src/estudos/testesswing/Calc.java
@@ -1,4 +1,40 @@
 package estudos.testesswing;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Calc {
+    private JLabel lblTitle;
+    private JTextField numberOne;
+    private JTextField numberTwo;
+    private JButton bntClick;
+    private JLabel lblResult;
+    private JLabel lblPlus;
+    private JPanel calcPanel;
+
+    public Calc() {
+        bntClick.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               float n1 = Float.parseFloat(numberOne.getText());
+               float n2 = Float.parseFloat(numberTwo.getText());
+               float soma = n1 + n2;
+               lblResult.setText(Float.toString(soma));
+
+
+            }
+        });
+    }
+
+    static void main(String[] args) {
+
+        JFrame frame = new JFrame("Calculadora");
+        frame.setContentPane(new Calc().calcPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
 }
